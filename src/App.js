bb8bcbe4f728b491eacf1e59880f6ad1d5fb0a19
mp-web3 <mp.web3@gmail.com>
2023-05-29 09:33:59 +0200

    fixed index homepage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter( createRoutesFromElements (
   <Route path='/' element={ <Root/> }>
-    <Route path='/' element={ <HomePage/> }/>
+    <Route index element={ <HomePage/> }/>
     <Route path=':type' element={ <HomePage/> }/>
     <Route path=':type/:id' element={ <PetDetailsPage/> }/>
     <Route path='search' element={ <SearchPage/> }/>
